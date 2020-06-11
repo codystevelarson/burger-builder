0e292f0e77e58679f743ef8dd939a6de8427b521
Cody Larson <cody.larson@fjorgedigital.com>
2020-06-11 17:15:06 -0500

    burger app build 1 complete

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    // This could be a functional component
+    componentWillUpdate() {
+        console.log("ORDER SUMMARY UPDATED")
+    }
+        
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
         return (
             <li key={igKey}> 
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span>:{props.ingredients[igKey]}
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>:{this.props.ingredients[igKey]}
             </li>
         )});
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>Burger Ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p>Continue to Checkout!</p>
-            <Button btnType='Danger' clicked={props.cancel}>CANCEL</Button>
-            <Button btnType='Success' clicked={props.continue}>CONTINUE</Button>
-        </Aux>
-    );
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>Burger Ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout!</p>
+                <Button btnType='Danger' clicked={this.props.cancel}>CANCEL</Button>
+                <Button btnType='Success' clicked={this.props.continue}>CONTINUE</Button>
+            </Aux>
+        );
+    }
+    
 }
 
-export default orderSummary;
+export default OrderSummary;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import Aux from '../../hoc/Aux';
+import Aux from '../../hoc/Aux/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
@@ -101,7 +101,8 @@ class BurgerBuilder extends Component {
                     <OrderSummary 
                         cancel={this.purchaseCancelHandler}
                         continue={this.purchaseContinueHandler}
-                        ingredients={this.state.ingredients}/>
+                        ingredients={this.state.ingredients}
+                        price={this.state.totalPrice}/>
                 </Modal>
 
                 <Burger ingredients={this.state.ingredients}/>
